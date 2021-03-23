ce4c13285fed853c9c2d5c825d0c2920074bcccf
Gabriel Gouvine <gabriel.gouvine_GIT@gmx.com>
2021-03-23 22:06:31 +0100

    Support loading pretrained RCAN models

--- models/rcan.py
+++ models/rcan.py
@@ -6,8 +6,16 @@
 import math
 import torch
 import torch.nn as nn
+from torchvision.models.utils import load_state_dict_from_url
 
 __all__ = [ 'rcan', 'rcan_g10r20f64', ]
+
+url = {
+    'g10r20f64x2': 'https://drive.google.com/uc?export=download&id=1aV7M0vaj0tmPHGFolXo6h23qXPPNIBgD',
+    'g10r20f64x3': 'https://drive.google.com/uc?export=download&id=1NfR1eqswNYMtegt5Et0I__L_2-MT2MXB',
+    'g10r20f64x4': 'https://drive.google.com/uc?export=download&id=1hMrnDTOTMOYHr-L9puMBqjqDkjeqKIbD',
+    'g10r20f64x8': 'https://drive.google.com/uc?export=download&id=1ovRngDYVTaOTp69TZb6nB-hT8ihyJFJo',
+}
 
 def default_conv(in_channels, out_channels, kernel_size, bias=True):
     return nn.Conv2d(
@@ -119,7 +127,7 @@ class ResidualGroup(nn.Module):
 
 ## Residual Channel Attention Network (RCAN)
 class RCAN(nn.Module):
-    def __init__(self, n_resgroups, n_resblocks, n_feats, reduction, scale, pretrained):
+    def __init__(self, n_resgroups, n_resblocks, n_feats, reduction, scale, pretrained=False):
         super(RCAN, self).__init__()
         if len(scale) != 1:
             raise ValueError("Exactly one scale is required for RCAN")
@@ -130,6 +138,12 @@ class RCAN(nn.Module):
         rgb_range = 255
         conv=default_conv
         act = nn.ReLU(True)
+        url_name = 'g{}r{}f{}x{}'.format(n_resgroups, n_resblocks, n_feats, scale)
+        if url_name in url:
+            self.url = url[url_name]
+        else:
+            self.url = None
+        self.filename = f"rcan_{url_name}.pt"
         
         # RGB mean for DIV2K
         self.sub_mean = MeanShift(rgb_range)
@@ -156,6 +170,9 @@ class RCAN(nn.Module):
         self.body = nn.Sequential(*modules_body)
         self.tail = nn.Sequential(*modules_tail)
 
+        if pretrained:
+            self.load_pretrained()
+
     def forward(self, x):
         x = self.sub_mean(255 * x)
         x = self.head(x)
@@ -167,6 +184,12 @@ class RCAN(nn.Module):
         x = self.add_mean(x) / 255
 
         return x 
+
+    def load_pretrained(self):
+        if self.url is None:
+            raise KeyError("No URL available for this model")
+        state_dict = load_state_dict_from_url(self.url, progress=True, file_name=self.filename)
+        self.load_state_dict(state_dict)
 
 
 def rcan_g10r20f64(scale, pretrained=False):
