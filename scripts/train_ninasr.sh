ed2f88864b77a2da7ec833285913f33c026ab78e
Gabriel Gouvine <gabriel.gouvine_GIT@gmx.com>
2021-04-20 23:12:34 +0200

    Move training script with the others + reduce learning rate for NinaSR-B2

--- scripts/train_ninasr.sh
+++ scripts/train_ninasr.sh
@@ -27,16 +27,16 @@ python main.py --tune-backend --lr 0.001 --patch-size-train 384 --epochs 100 --l
 
 # NinaSR-B2
 #   x2
-python main.py --tune-backend --lr 0.001 --patch-size-train  96 --epochs 300 --lr-decay-steps 200 280 --arch ninasr_b2 --scale 2 --log-dir logs/ninasr_b2_x2 --save-checkpoint ninasr_b2_x2.pt
+python main.py --tune-backend --lr 0.0001 --patch-size-train  96 --epochs 300 --lr-decay-steps 280 --arch ninasr_b2 --scale 2 --log-dir logs/ninasr_b2_x2 --save-checkpoint ninasr_b2_x2.pt
 #   x3
-python main.py --tune-backend --lr 0.001 --patch-size-train 144 --epochs 1 --lr-decay-steps 50 80 --freeze-backbone --arch ninasr_b2 --scale 3 --save-checkpoint ninasr_b2_x3.pt --load-pretrained ninasr_b2_x2_model.pt
-python main.py --tune-backend --lr 0.001 --patch-size-train 144 --epochs 100 --lr-decay-steps 50 80 --arch ninasr_b2 --scale 3 --log-dir logs/ninasr_b2_x3 --save-checkpoint ninasr_b2_x3.pt --load-pretrained ninasr_b2_x3_model.pt
+python main.py --tune-backend --lr 0.0001 --patch-size-train 144 --epochs 1 --lr-decay-steps 80 --freeze-backbone --arch ninasr_b2 --scale 3 --save-checkpoint ninasr_b2_x3.pt --load-pretrained ninasr_b2_x2_model.pt
+python main.py --tune-backend --lr 0.0001 --patch-size-train 144 --epochs 100 --lr-decay-steps 80 --arch ninasr_b2 --scale 3 --log-dir logs/ninasr_b2_x3 --save-checkpoint ninasr_b2_x3.pt --load-pretrained ninasr_b2_x3_model.pt
 #   x4
-python main.py --tune-backend --lr 0.001 --patch-size-train 192 --epochs 1 --lr-decay-steps 50 80 --freeze-backbone --arch ninasr_b2 --scale 4 --save-checkpoint ninasr_b2_x4.pt --load-pretrained ninasr_b2_x3_model.pt
-python main.py --tune-backend --lr 0.001 --patch-size-train 192 --epochs 100 --lr-decay-steps 50 80 --arch ninasr_b2 --scale 4 --log-dir logs/ninasr_b2_x4 --save-checkpoint ninasr_b2_x4.pt --load-pretrained ninasr_b2_x4_model.pt
+python main.py --tune-backend --lr 0.0001 --patch-size-train 192 --epochs 1 --lr-decay-steps 80 --freeze-backbone --arch ninasr_b2 --scale 4 --save-checkpoint ninasr_b2_x4.pt --load-pretrained ninasr_b2_x3_model.pt
+python main.py --tune-backend --lr 0.0001 --patch-size-train 192 --epochs 100 --lr-decay-steps 80 --arch ninasr_b2 --scale 4 --log-dir logs/ninasr_b2_x4 --save-checkpoint ninasr_b2_x4.pt --load-pretrained ninasr_b2_x4_model.pt
 #   x8
-python main.py --tune-backend --lr 0.001 --patch-size-train 384 --epochs 1 --lr-decay-steps 50 80 --freeze-backbone --arch ninasr_b2 --scale 8 --save-checkpoint ninasr_b2_x8.pt --load-pretrained ninasr_b2_x4_model.pt
-python main.py --tune-backend --lr 0.001 --patch-size-train 384 --epochs 100 --lr-decay-steps 50 80 --arch ninasr_b2 --scale 8 --log-dir logs/ninasr_b2_x8 --save-checkpoint ninasr_b2_x8.pt --load-pretrained ninasr_b2_x8_model.pt
+python main.py --tune-backend --lr 0.0001 --patch-size-train 384 --epochs 1 --lr-decay-steps 80 --freeze-backbone --arch ninasr_b2 --scale 8 --save-checkpoint ninasr_b2_x8.pt --load-pretrained ninasr_b2_x4_model.pt
+python main.py --tune-backend --lr 0.0001 --patch-size-train 384 --epochs 100 --lr-decay-steps 80 --arch ninasr_b2 --scale 8 --log-dir logs/ninasr_b2_x8 --save-checkpoint ninasr_b2_x8.pt --load-pretrained ninasr_b2_x8_model.pt
 
 
 
