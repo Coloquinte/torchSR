c94b246be1293778c8e2c3bb3adc349b77d239ed
Gabriel Gouvine <gabriel.gouvine_GIT@gmx.com>
2021-05-15 22:11:52 +0200

    Update README.md with more examples and results
    Results are computed on luminance channels with EDSR/DIV2K method, instead of RGB before

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
+# Evaluate on luminance channels, removing the border pixel; this is the convention used by most publications
 
 for scale in 2 3 4
 do
-    for arch in edsr_baseline edsr ninasr_b0 ninasr_b1 ninasr_b2 rcan rdn carn carn_m
+    for dataset in div2k_bicubic
     do
-	echo -n "${arch} x${scale}: "
-        python main.py --validation-only --arch $arch --scale $scale --chop-size 400 --download-pretrained
+        for arch in carn carn_m edsr_baseline edsr ninasr_b0 ninasr_b1 ninasr_b2 rcan rdn
+        do
+            echo -n "${dataset} ${arch} x${scale}: "
+            python main.py --validation-only --arch $arch --scale $scale --dataset-val $dataset --chop-size 400 --download-pretrained --shave-border $scale --eval-luminance
+        done
     done
 done
 
+for scale in 8
+do
+    for dataset in div2k_bicubic
+    do
+        for arch in ninasr_b0 ninasr_b1 ninasr_b2 rcan
+        do
+            echo -n "${dataset} ${arch} x${scale}: "
+            python main.py --validation-only --arch $arch --scale $scale --dataset-val $dataset --chop-size 400 --download-pretrained --shave-border $scale --eval-luminance
+        done
+    done
+done
