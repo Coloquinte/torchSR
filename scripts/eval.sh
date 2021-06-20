2ce78e072a8998416005e0cb8bc76ae6403819b0
Gabriel Gouvine <gabriel.gouvine_GIT@gmx.com>
2021-06-20 20:05:33 +0200

    Update evaluation script to use replication padding for NinaSR

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -5,7 +5,7 @@ for scale in 2 3 4
 do
     for dataset in div2k_bicubic
     do
-        for arch in carn carn_m edsr_baseline edsr ninasr_b0 ninasr_b1 ninasr_b2 rcan rdn
+        for arch in carn carn_m edsr_baseline edsr rcan rdn
         do
             echo -n "${dataset} ${arch} x${scale}: "
             python -m torchsr.train --validation-only --arch $arch --scale $scale --dataset-val $dataset --chop-size 400 --download-pretrained --shave-border $scale --eval-luminance
@@ -13,14 +13,14 @@ do
     done
 done
 
-for scale in 8
+for scale in 2 3 4 8
 do
     for dataset in div2k_bicubic
     do
-        for arch in ninasr_b0 ninasr_b1 ninasr_b2 rcan
+        for arch in ninasr_b0 ninasr_b1 ninasr_b2
         do
             echo -n "${dataset} ${arch} x${scale}: "
-            python -m torchsr.train --validation-only --arch $arch --scale $scale --dataset-val $dataset --chop-size 400 --download-pretrained --shave-border $scale --eval-luminance
+            python -m torchsr.train --validation-only --arch $arch --scale $scale --dataset-val $dataset --chop-size 400 --download-pretrained --shave-border $scale --eval-luminance --replication-pad 4
         done
     done
 done
