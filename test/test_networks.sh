d6c965c764f17b85a793e7957f13b897488e4c4b
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-12-15 15:35:30 +0000

    More fix

--- test/test_networks.sh
+++ test/test_networks.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-pwd
-ls -l
-ls -l ..
 for arch in edsr_baseline edsr ninasr_b0 ninasr_b1 ninasr_b2 rdn rcan carn carn_m
 do
-	python main.py --arch $arch --scale 2 --download-pretrained --validation-only --dataset-val set5
+	python3 -m torchsr.train --arch $arch --scale 2 --download-pretrained --validation-only --dataset-val set5
 done
